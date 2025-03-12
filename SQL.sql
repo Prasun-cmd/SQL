272e34669a6446c9e8fb283153f630cf424ba7b9
Prasun-cmd <24f1000525@ds.study.iitm.ac.in>
2025-03-12 14:35:17 +0530

    Create SQL.sql

---
+++ SQL.sql
@@ -0,0 +1,18 @@
+--synatax create table tablename(column1 datatype constraint, column2 datatype constraint,column3 datatype constraint);
+
+--Datatype
+1.char(n): Fixed-length string of n characters.
+2.varchar(n): Variable-length string upto n characters.
+3.Date: 4 byte integer.
+4.int: store calander dates.
+5.Boolean: Holds True,False or Null
+
+--Constraints
+1.NULL: Allows missing values.
+2.NOT NULL: Disallows missing values.
+3.UNIQUE: Ensure all value distinct.
+4.CHECK: Ensure value meets the condition.
+5.PRIMARY KEY: Unique identifier for table(combine NOT NULL and UNIQUE).
+6.FOREIGN KEY: Links to a primary key for data integrity.
+
+
